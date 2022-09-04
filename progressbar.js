c83d2031f2adb6940a0db109cf453aa144bdbcc1
Sergey Bezdenezhnykh <seregabdx@gmail.com>
2022-09-04 15:17:24 +0300

    Added input block

--- progressbar.js
+++ progressbar.js
@@ -14,8 +14,23 @@ const ProgressBar = (function () {
       progressBar.setAttribute("width", "120px");
       progressBar.innerHTML = `<circle class="progressRing" stroke="white" stroke-width="4" cx="60" cy="60" r="52" fill="transparent" />`;
       progressBarContainer.appendChild(progressBar);
-      let circle = progressBarContainer.querySelector(".progressRing");
-      circle.style.strokeDashArray = "500 20";
+      this.progressRing = progressBarContainer.querySelector(".progressRing");
+      this.progressRing.style.width = "30px";
+      this.progressRing.style.height = "30px";
+      this.longCircle = this.progressRing.r.baseVal.value * 2 * Math.PI;
+      this.progressRing.setAttribute(
+        "stroke-dasharray",
+        `${this.longCircle} ${this.longCircle}`
+      );
+      this.progressRing.strokeDashoffset = this.longCircle;
+      this.progressRing.setAttribute("transform", "rotate(-90)");
+      this.progressRing.setAttribute("transform-origin", "center");
+    }
+
+    changeValue(value) {
+      let offset = this.longCircle - (value / 100) * this.longCircle;
+      this.progressRing.style.strokeDashoffset = offset;
+      console.log(offset);
     }
   }
 
--- script.js
+++ script.js
@@ -1,3 +1,46 @@
-"use stict";
+"use strict";
 
 let progressBar = new ProgressBar("progressBar");
+let animSwitch = false;
+let disSwitch = false;
+let value = 0;
+
+let input = document.querySelector(".inputPercent");
+input.oninput = (e) => {
+  if (e.target.value > 100) {
+    e.target.value = 100;
+  }
+
+  value = e.target.value;
+  //console.log(value);
+  progressBar.changeValue(e.target.value);
+};
+
+let animateSwitch = document.querySelectorAll(".switch-container-switch")[0];
+animateSwitch.onchange = () => {
+  animSwitch = !animSwitch;
+  if (animSwitch) {
+    let interval = setInterval(() => {
+      if (!animSwitch) {
+        clearInterval(interval);
+      }
+
+      value += 1;
+      if (value == 101) {
+        value = 0;
+      }
+      progressBar.changeValue(value);
+    }, 100);
+  }
+};
+
+let disableSwitch = document.querySelectorAll(".switch-container-switch")[1];
+let progressRing = document.querySelector(".progress");
+disableSwitch.onchange = () => {
+  disSwitch = !disSwitch;
+  if (disSwitch) {
+    progressRing.style.visibility = "hidden";
+  } else {
+    progressRing.style.visibility = "visible";
+  }
+};
